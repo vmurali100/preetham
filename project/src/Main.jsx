9ae6a0526da5b5f4004e1044f5181dfd829a2acd
vmurali100 <vmurali100@gmail.com>
2020-11-14 12:26:09 +0530

    Added

---
+++ project/src/Main.jsx
@@ -0,0 +1,172 @@
+import React from "react";
+import { SideNavbar } from "./SideNavbar";
+
+export const Main = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-2">
+          <SideNavbar />
+        </div>
+        <div className="col-md-10">
+          <div className="row">
+            <div className="col-md-7">
+              <div className="row">
+                <div className="col-md-8">
+                  <div class="card mb-2 mt-2 shadow-sm">
+                    <div class="card-header">
+                      <h4 class="my-0 font-weight-normal">Pro</h4>
+                    </div>
+                    <div class="card-body">
+                      <ul class="list-unstyled mt-1 mb-1">
+                        <li>20 users included</li>
+                        <li>10 GB of storage</li>
+                        <li>Priority email support</li>
+                        <li>Help center access</li>
+                      </ul>
+                      <button
+                        type="button"
+                        class="btn btn-lg btn-block btn-primary"
+                      >
+                        Get started
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-4">
+                  <div class="card mb-2 mt-2 shadow-sm">
+                    <div class="card-header">
+                      <h4 class="my-0 font-weight-normal">Pro</h4>
+                    </div>
+                    <div class="card-body">
+                      <ul class="list-unstyled mt-1 mb-1">
+                        <li>20 users included</li>
+                        <li>10 GB of storage</li>
+                        <li>Priority email support</li>
+                        <li>Help center access</li>
+                      </ul>
+                      <button
+                        type="button"
+                        class="btn btn-lg btn-block btn-primary"
+                      >
+                        Get started
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col">
+                  <div class="card mb-2 shadow-sm">
+                    <div class="card-header">
+                      <h4 class="my-0 font-weight-normal">Pro</h4>
+                    </div>
+                    <div class="card-body">
+                      <ul class="list-unstyled mt-1 mb-1">
+                        <li>20 users included</li>
+                        <li>10 GB of storage</li>
+                        <li>Priority email support</li>
+                        <li>Help center access</li>
+                      </ul>
+                      <button
+                        type="button"
+                        class="btn btn-lg btn-block btn-primary"
+                      >
+                        Get started
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col">
+                  <div class="card mb-4 shadow-sm">
+                    <div class="card-header">
+                      <h4 class="my-0 font-weight-normal">Pro</h4>
+                    </div>
+                    <div class="card-body">
+                      <ul class="list-unstyled mt-1 mb-1">
+                        <li>20 users included</li>
+                        <li>10 GB of storage</li>
+                        <li>Priority email support</li>
+                        <li>Help center access</li>
+                      </ul>
+                      <button
+                        type="button"
+                        class="btn btn-lg btn-block btn-primary"
+                      >
+                        Get started
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className="col">
+                  {" "}
+                  <div class="card mb-4 shadow-sm">
+                    <div class="card-header">
+                      <h4 class="my-0 font-weight-normal">Pro</h4>
+                    </div>
+                    <div class="card-body">
+                      <ul class="list-unstyled mt-1 mb-1">
+                        <li>20 users included</li>
+                        <li>10 GB of storage</li>
+                        <li>Priority email support</li>
+                        <li>Help center access</li>
+                      </ul>
+                      <button
+                        type="button"
+                        class="btn btn-lg btn-block btn-primary"
+                      >
+                        Get started
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-5">
+              <div class="card mb-4 mt-2 shadow-sm">
+                <div class="card-header">
+                  <h4 class="my-0 font-weight-normal">Pro</h4>
+                </div>
+                <div class="card-body">
+                  <ul class="list-unstyled mt-3 mb-4">
+                    <li>20 users included20 users included20 users included</li>
+                    <li>20 users included20 users included20 users included</li>
+                    <li>20 users included20 users included20 users included</li>
+                    <li>20 users included20 users included20 users included</li>
+                    <li>20 users included20 users included20 users included</li>
+                    <li>20 users included20 users included20 users included</li>
+                    <li>20 users included20 users included20 users included</li>
+                    <li>20 users included20 users included20 users included</li>
+                    <li>20 users included20 users included20 users included</li>
+                    <li>20 users included20 users included20 users included</li>
+                    <li>20 users included20 users included20 users included</li>
+                    <li>20 users included20 users included20 users included</li>
+                    <li>20 users included20 users included20 users included</li>
+                    <li>20 users included20 users included20 users included</li>
+                    <li>20 users included20 users included20 users included</li>
+                    <li>20 users included20 users included20 users included</li>
+                    <li>20 users included20 users included20 users included</li>
+                    <li>
+                      20 users included20 users included20 users included
+                    </li>{" "}
+                    <li>20 users included20 users included20 users included</li>
+                    <li>20 users included20 users included20 users included</li>
+                    <li>20 users included20 users included20 users included</li>
+                  </ul>
+                  <button
+                    type="button"
+                    class="btn btn-lg btn-block btn-primary"
+                  >
+                    Get started
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
